b0e202ee7c13e32fcf32a09c270cdaa1f76f8811
feDann <daniele99@ferrazzo.it>
2020-03-20 19:29:12 +0100

    Oh mia bella madunninaah pt2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("FRA CULO !");
-        System.out.println("DANI GHEI");
         System.out.println("FRA PIU' CULO");
     }
 
