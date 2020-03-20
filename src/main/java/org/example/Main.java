082436f5305716af40c3d60cec74a1bd0c32fa86
feDann <daniele99@ferrazzo.it>
2020-03-20 19:24:41 +0100

    Oh mia bella madunninaah

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("FRA CULO !");
         System.out.println("DANI GHEI");
+        System.out.println("FRA PIU' CULO");
     }
 
 }
